322dcf6087f1809659ca3174933a68cd54bf4ba3
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2022-06-14 03:46:37 -0300

    Minor fix

--- app/subscriptions/models.py
+++ app/subscriptions/models.py
@@ -9,15 +9,23 @@ from pydantic import BaseModel
 class SubscriptionType:
     name: str
     price: Decimal
+    level: int
 
-    def __init__(self, name: str, price: Decimal):
+    def __init__(self, name: str, price: Decimal, level: int):
         self.name = name
         self.price = price
+        self.level = level
 
 
-basic = SubscriptionType("Basic", Decimal("0.000001"))
-pro = SubscriptionType("Pro", Decimal("0.000002"))
-premium = SubscriptionType("Premium", Decimal("0.000003"))
+basic = SubscriptionType("Basic", Decimal("0.000001"), 1)
+pro = SubscriptionType("Pro", Decimal("0.000002"), 2)
+premium = SubscriptionType("Premium", Decimal("0.000003"), 3)
+
+
+def _get_subscription_level(s_type: str):
+    for s in [basic, pro, premium]:
+        if s.name == s_type:
+            return s.level
 
 
 def _validate_subscription_type_id(t: str):
@@ -32,12 +40,14 @@ class UserSubscriptionModel:
     id: str
     user_id: str
     subscription_type_id: str
+    subscription_type_level: int
     date_subscribed: datetime.datetime
     last_payed: datetime.datetime
 
     def __init__(self, user_id: str, subscription_type_id: str):
         self.user_id = user_id
         self.subscription_type_id = subscription_type_id
+        self.subscription_type_level = _get_subscription_level(subscription_type_id)
         self.date_subscribed = datetime.datetime.today()
         self.last_payed = datetime.datetime.today()
 
@@ -45,6 +55,7 @@ class UserSubscriptionModel:
         r = {
             "user_id": self.user_id,
             "subscription_type_id": self.subscription_type_id,
+            "subscription_type_level": self.subscription_type_level,
             "date_subscribed": self.date_subscribed,
             "last_payed": self.last_payed,
         }
@@ -57,6 +68,8 @@ class UserSubscriptionModel:
     @classmethod
     def from_mongo(cls, m):
         m["id"] = str(m.pop("_id"))
+        if "subscription_type_level" in m:
+            m.pop("subscription_type_level")
         return m
 
 
